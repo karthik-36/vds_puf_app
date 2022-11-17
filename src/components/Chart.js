7df374a6cc018becde2d4382ab28e95ef2968d3d
jasonpereira96 <jasonpereira074@gmail.com>
2022-11-17 14:47:18 -0600

    adding validations and fixing params

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -25,7 +25,7 @@ class Chart extends React.Component {
     let chart;
     switch(this.props.chartType) {
       case ChartTypes.BAR: {
-        chart = BarChart(this.props.chartOptions);
+        chart = BarChart(this.props.chartData, this.props.chartOptions);
       } break;
       case ChartTypes.HISTOGRAM: {
         chart = Histogram(this.props.chartData, this.props.chartOptions);
@@ -40,7 +40,8 @@ class Chart extends React.Component {
 
 Chart.propTypes = {
   chartType: PropTypes.oneOf(Object.keys(ChartTypes)).isRequired,
-  chartOptions: PropTypes.object.isRequired
+  chartOptions: PropTypes.object.isRequired,
+  chartData: PropTypes.array.isRequired
 };
 
 export default Chart;
